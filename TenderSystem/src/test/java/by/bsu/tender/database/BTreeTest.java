4dae53ddbfc946bc246cd99a906bbf2320f0df1d
Sergey Zosimov <sergeeys@tut.by>
2015-03-07 20:50:28 +0300

    [+] add test for some classes

---
+++ TenderSystem/src/test/java/by/bsu/tender/database/BTreeTest.java
@@ -0,0 +1,24 @@
+package by.bsu.tender.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BTreeTest {
+
+	@Test
+	public void testAddTender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindTender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteTender() {
+		fail("Not yet implemented");
+	}
+
+}
